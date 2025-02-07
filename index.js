5ccdb7260fbc9065987961ad081348b33a73378a
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-08 01:17:51 +0530

    Hi api cretaed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("frist server created")
+import express from 'express'
+
+const server = express();
+
+server.get("/hi",( req, res) =>{
+    res.send("hellow")
+});
+server.listen(5000);
