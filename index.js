38d9786ce41db0ec6f0d27a50a4c2a76b2576085
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-08 01:49:30 +0530

    Understand the nodemon its a file wathcer which sver to autoreloed when some cahnges occers in files

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from 'express'
 
 const app = express();
+
+app.get("/home",(req, res)=>{
+    res.send("hellow i'm on Home Page ")
+})
+app.get("/about",(req, res)=>{
+    res.send("hellow i'm on About page ")
+})
+app.get("/hi",(req, res)=>{
+    res.send("Bye")
+})
 const Port = 5001;
 app.listen(Port,()=>
 {
