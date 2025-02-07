792f2819a9956c85321c2b3dd656be2a60938819
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-08 01:23:31 +0530

    Created Basic serrver

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import express from 'express'
 const server = express();
 
 server.get("/hi",( req, res) =>{
-    res.send("hellow")
+    res.send("hellow Our first server created good ")
 });
 server.listen(5000);
