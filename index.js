1c0d0a40f67ea0da45244908af165a7a2be3508b
rupesh3201 <rupesh.mandge3201@gmail.com>
2025-02-08 19:26:14 +0530

    Printed hole req obej

--- index.js
+++ index.js
@@ -3,29 +3,31 @@ import express from 'express'
 const app = express();
 // app listing on something
 app.get("/home",(req, res)=>{
+    console.log(req);
+    
     res.send("hellow i'm on Home Page ")
 })
 // get is method  /about is route and endpoint controller its has two objet req and res
-app.get("/about",(req, res)=>{
-    res.send("hellow i'm on About page ")
-})
-app.get("/hi",(req, res)=>{
-    res.send("Bye")
-})
-app.get("/result",(req, res)=>{
-    console.log(req);
-    console.log(res);
-    res.json({
-        sucess : true ,
-        message :" result ",
-        data : {
-            class : 10 ,
-            sub : "Hindi" ,
-            marks : 85   ,
-            result: "PASS"
-        }
-    })
-})
+// app.get("/about",(req, res)=>{
+//     res.send("hellow i'm on About page ")
+// })
+// app.get("/hi",(req, res)=>{
+//     res.send("Bye")
+// })
+// app.get("/result",(req, res)=>{
+//     console.log(req);
+//     console.log(res);
+//     res.json({
+//         sucess : true ,
+//         message :" result ",
+//         data : {
+//             class : 10 ,
+//             sub : "Hindi" ,
+//             marks : 85   ,
+//             result: "PASS"
+//         }
+//     })
+// })
 const Port = 5001;
 app.listen(Port,()=>
 {
